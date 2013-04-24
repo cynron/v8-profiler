bce7b2b159e060f8a58bdafa3a25ea5b960a2cfc
Stephen Belanger <cyruzdraxs@gmail.com>
2013-04-24 11:10:29 -0700

    Remove unused m_snapshot value from header

--- snapshot.h
+++ snapshot.h
@@ -17,8 +17,6 @@ class Snapshot {
     Snapshot(const v8::HeapSnapshot* snapshot)
         : m_snapshot(snapshot){}
 
-    const v8::HeapSnapshot* m_snapshot;
-
     static Handle<Value> GetUid(Local<String> property, const AccessorInfo& info);
     static Handle<Value> GetTitle(Local<String> property, const AccessorInfo& info);
     static Handle<Value> GetRoot(Local<String> property, const AccessorInfo& info);
